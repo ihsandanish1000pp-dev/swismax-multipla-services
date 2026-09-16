8c7dd6c9b043e0e4bd838a2a5dd5a81d737aa534
Your Name <your@email.com>
2026-09-16 15:56:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+function sendMessage(event) {
+
+            // Stop page refresh
+            event.preventDefault();
+
+
+            // Get user name
+            let name =
+                document.getElementById("name").value;
+
+
+            // Get selected service
+            let service =
+                document.getElementById("service").value;
+
+
+            // Show success message
+            document.getElementById("message").innerHTML =
+                "Thank you " + name +
+                "! Your request for " +
+                service +
+                " has been received.";
+
+
+            // Clear form
+            document.querySelector("form").reset();
+
+
+            // Remove message after 3 seconds
+            setTimeout(function () {
+
+                document.getElementById("message").innerHTML = "";
+
+            }, 3000);
+
+        }
